702b753bb95a441cdcb1a9c6504e3d947956557f
Prateek Shukla <prateekshukla978@gmail.com>
2023-12-09 13:03:38 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+### Introduction
+
+Welcome to the Recipe Finder project! This web application allows users to search for recipes using a search box that queries an API. The search results are displayed in cards, and users can click on a card to view more details about a specific recipe in a modal.
+
+### Features
+
+1. Search Box: Users can enter a term in the search box to find recipes.
+
+2. Recipe Cards: Search results are presented as cards, each containing essential information about a recipe, such as its name, image, and a brief description.
+
+3. Modal View: Clicking on a recipe card opens a modal that provides detailed information about the selected recipe, including ingredients and instructions.
+
+### Technologies Used
+
+HTML, CSS, JavaScript: The core languages used for building the web application.
+
+Bootstrap: Utilized for styling components, including the modal and responsive design.
+
+API Integration: Fetches recipe data from a recipe API based on user input.
+
+### How to Use
+
+Search for Recipes:
+Enter a term in the search box to find recipes related to that term.
+
+View Recipe Details:
+Click on a recipe card to view more details about the selected recipe in a modal.
+
+API Information
+This project uses Recipe API to fetch recipe data. You may need to sign up for an API key to use the service.
+
+Project Structure
+index.html: The main HTML file containing the structure of the web page.
+
+styles.css: The CSS file for styling the web page.
+
+script.js: The JavaScript file that handles user interactions, API requests, and modal functionality.
+
+Contributing
+Feel free to contribute to the project by submitting issues or pull requests. Your feedback and suggestions are highly appreciated!
---
+++ script.js
@@ -0,0 +1,89 @@
+async function findRecipe() {
+  let recipe = document.getElementById("searchInput").value;
+  console.log(recipe);
+  const url =
+    "https://api.edamam.com/search?q=" +
+    recipe +
+    "&app_id=62175d8b&app_key=2216b2fa6b85b5a7f25b112e694abcd4";
+  const response = await fetch(url);
+  const recipies = await response.json();
+  showRecipeData(recipies.hits);
+}
+
+function showRecipeData(data) {
+  const cardString = data.map(
+    (card) => `
+    <div class="card col-md-3" style="width: 18rem">
+    <img src="${card.recipe.image}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${card.recipe.label}</h5>
+      <p class="card-text">
+        Source : ${card.recipe.source}
+      </p>
+      <div class="text-center my-3">
+      <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#myModal"
+      onclick='checkFunc(${JSON.stringify(card.recipe)})'
+      data-toggle="modal"
+      data-target="#myModal"
+    >
+      Open Modal
+    </button>
+    </div>
+    </div>
+  </div>`
+  );
+
+  document.getElementById("recipies").innerHTML = cardString;
+}
+
+function checkFunc(cardModal) {
+  console.log(cardModal.label);
+  showModal(cardModal);
+}
+
+function showModal(modalData) {
+  var modelDiv = `<div
+  class="modal fade"
+  id="myModal"
+  tabindex="-1"
+  role="dialog"
+  aria-labelledby="exampleModalLabel"
+  aria-hidden="true"
+>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">${modalData.label}</h5>
+        <button
+          type="button"
+          class="close"
+          data-dismiss="modal"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      
+      <img src="${modalData.image}" class="card-img-top" alt="..." />
+        <p>Clories : ${modalData.calories}</p>
+        <p>Weight : ${modalData.totalWeight}</p>
+      </div>
+      <div class="modal-footer">
+        <button
+          type="button"
+          class="btn btn-secondary"
+          data-dismiss="modal"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</div>`;
+  document.getElementById("modal-div").innerHTML = modelDiv;
+}
